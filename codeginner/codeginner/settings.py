629963e4d7bc8e2a4df79ac80b4bc51fb2af5283
siddhu750 <85008297+siddhu750@users.noreply.github.com>
2023-04-22 22:55:29 +0530

    Update settings.py

--- codeginner/codeginner/settings.py
+++ codeginner/codeginner/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'codeginner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'database-1.crhwi9v2pxsp.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
